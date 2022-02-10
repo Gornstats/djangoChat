6bf52fcda6d632f819e4a9ba7ccbde322c771890
Jeremy Gorniak <railspider@gmail.com>
2022-02-10 22:59:29 +1100

    Added user signup & login feature

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from .views import frontpage, signup
@@ -5,4 +6,6 @@ from .views import frontpage, signup
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
